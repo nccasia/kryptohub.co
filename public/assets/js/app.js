4cdec9eeba7a70a6ad8cdf283b801fd9523494dd
Hoai Thanh <thanh.maihoai@ncc.asia>
2022-12-11 08:06:53 +0700

    update UI...

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -32,6 +32,8 @@ function ourTeamSliderControl() {
       .slick({
         dots: true,
         infinite: false,
+        autoplay: true,
+        mobileFirst: true,
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
@@ -60,7 +62,6 @@ function ourTeamSliderControl() {
     }, 0);
   }
 }
-
 function ourTeamModalControl() {
   const members = [
     {
@@ -321,12 +322,88 @@ function ourProjectControl() {
   });
 }
 
+function sendMail() {
+  function printMess(elemId, message) {
+    document.getElementById(elemId).innerHTML = message;
+  }
+
+  const formEl = document.querySelector(".form");
+  formEl.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
+    const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
+    const content = document.getElementById("content").value;
+
+    if (
+      firstName === "" ||
+      lastName === "" ||
+      email === "" ||
+      phone === "" ||
+      content === ""
+    ) {
+      document.getElementById("nameMiss").innerHTML =
+        "Please fill out all required fields.";
+      return;
+    }
+
+    Array.from(formEl.elements).forEach(
+      (formElement) => (formElement.disabled = true)
+    );
+    const data = {
+      email: document.getElementById("email").value,
+      content: document.getElementById("content").value,
+    };
+    const btnSubmit = formEl.querySelector('button[type="submit"]');
+    const btnLoading = formEl.querySelector('button[type="button"]');
+    btnSubmit.classList.add("d-none");
+    btnLoading.classList.remove("d-none");
+
+    fetch("https://email.ncc.asia/ncc-site-api-sendmail", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => {
+        if (!res.ok) {
+          return res.text().then((text) => {
+            throw new Error(text);
+          });
+        } else {
+          printMess(
+            "nameSuccess",
+            "Thank you, your submission has been received."
+          );
+          document.getElementById("nameMiss").remove();
+        }
+      })
+      .catch((err) => {
+        printMess(
+          "nameError",
+          "Oops, something went wrong. Please try again later."
+        );
+      })
+      .finally(() => {
+        formEl.reset();
+        Array.from(formEl.elements).forEach(
+          (formElement) => (formElement.disabled = false)
+        );
+        btnSubmit.classList.remove("d-none");
+        btnLoading.classList.add("d-none");
+      });
+  });
+}
+
 function main() {
   toggleNavbarMobile();
   ourTeamSliderControl();
   ourTeamModalControl();
   ourTeamModalControlJP();
   ourProjectControl();
+  sendMail();
 }
 
 main();
